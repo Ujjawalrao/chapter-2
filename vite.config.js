a0852cc5f2efa51cff9690b77015aa88091c9e99
Ujjawal Yadav <147393190+Ujjawalrao@users.noreply.github.com>
2025-05-08 17:42:39 +0530

    Add files via upload
    files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import nesting from 'tailwindcss/nesting'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        nesting,
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
+}) 
